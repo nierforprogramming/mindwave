c886eaa6fa20e6d9420ce1d1035ef4d47d486737
Niraj <nierforprogramming@gmail.com>
2026-02-20 14:53:54 +0545

    Header component completed, assets, constants added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="pt-[4.7rem] lg:pt-[5.5rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
 };
 
 export default App;
