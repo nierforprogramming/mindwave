e821b32f4cb5be01bda4f0e4aa3bae582feb63e9
Niraj <nierforprogramming@gmail.com>
2026-02-24 15:30:25 +0545

    fixed background color in chat logo

--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -28,7 +28,7 @@ export const VideoChatMessage = () => {
   return (
     <div className="absolute top-8 left-[3.125rem] w-full max-w-[14rem] pt-2.5 pr-2.5 pb-7 pl-5 bg-n-6 rounded-t-xl rounded-br-xl font-code text-base md:max-w-[17.5rem]">
       Video generated!
-      <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1 rounded-[0.75rem]">
+      <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1/15 rounded-[0.75rem]">
         <img src={mindwaveSymbol} width={26} height={26} alt="Mindwave" />
       </div>
       <p className="tagline absolute right-2.5 bottom-1 text-[0.625rem] text-n-3 uppercase">
