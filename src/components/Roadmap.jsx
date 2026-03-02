52db2f096c9bdb2f33a2c1f2c493e64fece9f637
Niraj <nierforprogramming@gmail.com>
2026-03-02 12:52:48 +0545

    Roadmap section completed

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./TagLine";
 import { roadmap } from "../constants";
-import { check2, grid, loading1 } from "../../public/assets";
+import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
+import Tagline from "./Tagline";
 
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
